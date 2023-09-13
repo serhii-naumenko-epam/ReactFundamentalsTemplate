f3716a45ffef87d8d0f3aa4e91efe803b61cc8db
Serhii_Naumenko <serhii_naumenko@epam.com>
2023-09-13 22:57:01 +0300

    Fix date

--- src/helpers/formatCreationDate.js
+++ src/helpers/formatCreationDate.js
@@ -1,7 +1,12 @@
-export const formatCreationDate = (date) => {
-  
-  const parts = date.split('/');
-  if (parts.length !== 3) return null;
-  
-  return `${parts[0]}.${parts[1]}.${parts[2]}`;
+export const formatCreationDate = (enteredDate) => {
+
+  const date = new Date(enteredDate);
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  const formattedDate = `${day}.${month}.${year}`;
+
+  return formattedDate;
 }
